aba43e22fc510d39ab057136dfd3103310c0d813
fuguilin <fuguilin@axzo.cn>
2020-05-21 10:18:53 +0800

    ....

--- bind.js
+++ bind.js
@@ -1,13 +1,15 @@
 /*
  * @Author: your name
  * @Date: 2020-05-20 11:24:57
- * @LastEditTime: 2020-05-21 10:08:06
+ * @LastEditTime: 2020-05-21 10:18:44
  * @LastEditors: Please set LastEditors
  * @Description: .call()和.apply()方法的区别除了第二个传的参数不一样以外，作用都是改变this的指向，然后执行函数
+ *               也就是对象调用一个函数
  *               第一个参数如果不传就是当前的执行环境 window/global
  * @FilePath: /axzo-oms/Users/fgl/functional/bind.js
  */
 
+// .call()第二个参数是可以传入多个参数的   .apply 的第二个参数是数组
 Function.prototype.call2 = function (context = window, ...args) {
   console.log(this);
   console.log(context);
